1525f930959bc08267358bc2fc9c8670b04b155b
Cam Spiers <camspiers@gmail.com>
2014-06-11 17:00:11 +1200

    More thorough unit tests for v8

--- tests/cases/V8JsRendererTest.php
+++ tests/cases/V8JsRendererTest.php
@@ -4,18 +4,64 @@ namespace ReactJS;
 
 class V8JsRendererTest extends \PHPUnit_Framework_TestCase
 {
+    protected $v8;
+    
+    public function setUp()
+    {
+        $this->v8 = new \V8Js;
+    }
+
     public function testRenderComponentToString()
     {
         $r = new V8JsRenderer(
-            new \V8Js,
+            $this->v8,
             [
                 __DIR__ . '/../fixtures/bundle.js'
             ]
         );
         
+        $markup = $r->renderComponentToString('./TestComponent');
+        
         $this->assertContains(
             'Some testing content',
-            $r->renderComponentToString('./TestComponent')
+            $markup
+        );
+
+        $this->assertContains(
+            'data-reactid',
+            $markup
+        );
+
+        $this->assertContains(
+            'data-react-checksum',
+            $markup
+        );
+    }
+    
+    public function testRenderComponentToStaticMarkup()
+    {
+        $r = new V8JsRenderer(
+            $this->v8,
+            [
+                __DIR__ . '/../fixtures/bundle.js'
+            ]
+        );
+        
+        $markup = $r->renderComponentToStaticMarkup('./TestComponent');
+
+        $this->assertContains(
+            'Some testing content',
+            $markup
+        );
+
+        $this->assertNotContains(
+            'data-reactid',
+            $markup
+        );
+
+        $this->assertNotContains(
+            'data-react-checksum',
+            $markup
         );
     }
 }
